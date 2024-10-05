b65f2ee3b500392a8957bc0c5b6f068c1992cea2
Ana Barreto <acobarbosa65@gmail.com>
2024-10-04 23:17:32 -0300

    Desafio POO com C# finalizado!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia meuNokia = new Nokia("999888777", "G60", "000000-00-000000-0", 128);
+meuNokia.Ligar();
+meuNokia.ReceberLigacao();
+meuNokia.InstalarAplicativo("Telegram");
+
+Iphone meuIphone = new Iphone("999222333", "iPhone6", "000000-00-000000-2", 32);
+meuIphone.Ligar();
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("Duolingo");
